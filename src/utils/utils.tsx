71dda58baff5f55f0e11340f0c89f0194e294ad0
Dieter Mutiz <dieter_md@hotmail.com>
2023-07-09 22:33:40 -0300

    Header: fetching movies from the api

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,1 +1,15 @@
-export const ImagesBaseURL: string = "https://image.tmdb.org/t/p/original"
+import { Movie } from "../models/movie";
+import { API_KEY } from "./apiKey";
+
+const ImagesBaseURL: string = "https://image.tmdb.org/t/p/original"
+
+export const GetImageUrl = (imgPath: string): string  => ImagesBaseURL + imgPath;
+
+export const FetchUpcomingMovies = async () => {
+  const response = await fetch(`https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1&api_key=${API_KEY}`)
+  const json = await response.json()
+  const movies = json.results as Movie[]
+  return movies
+}
+
+
