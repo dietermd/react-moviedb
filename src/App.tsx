3604b18377fde6247cd2e024c9db6123bc65daa6
Dieter Mutiz <dieter_md@hotmail.com>
2023-07-14 21:28:10 -0300

    Banner component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
+import Banner from "./components/bannner"
 import Header from "./components/header"
 
 function App() {
 
   return (
     <>
-      <Header />
-      {/* <div className="flex flex-row">
-        <div className="basis-1/12">01</div>
-        <div className="basis-10/12 border-2 border-red-600">02</div>
-        <div className="basis-1/12">03</div>
-      </div> */}
+      <div className="flex justify-center">
+        <div className="w-full lg:w-10/12">
+          <Header />
+          <Banner />
+        </div>
+      </div>
+      
     </>
 
   )
