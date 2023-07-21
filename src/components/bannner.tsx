53201641cf29fcfe0ab8aeea6cde52484770f630
Dieter Mutiz <dieter_md@hotmail.com>
2023-07-20 23:37:55 -0300

    Banner loading and genres

--- src/components/bannner.tsx
+++ src/components/bannner.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { Movie } from "../models/movie"
-import { FetchUpcomingMovies, GetImageUrl } from "../utils/utils"
+import { FetchUpcomingMovies, GetImageUrl, Genres } from "../utils/utils"
 import { Swiper, SwiperSlide } from "swiper/react"
 import { Pagination } from "swiper/modules"
 
@@ -18,26 +18,39 @@ export default function Banner() {
 
   return (
     movies.length === 0 ?
-    <div className="h-[600px] w-full flex justify-center items-center">
-      <h1>Fetching upcoming movies...</h1>
-    </div>        
+    RenderLoading()
     :
     RenderMovieCarousel(movies) 
+  )
+}
+
+function RenderLoading() {
+  return (
+    <div className="relative animate-pulse h-[600px] w-full bg-slate-300 rounded-b-lg"></div>
   )
 }
 
 function RenderMovieCarousel(movies: Movie[]) {  
   return (
     <>
-      <Swiper pagination={true} modules={[Pagination]} 
-        className="h-[600px] w-full">
+      <Swiper pagination={true} modules={[Pagination]}
+        className="banner-swiper h-[600px] w-full rounded-b-lg">
         {
           movies.map((movie, i) =>
             <SwiperSlide key={i}>
-              <img src={GetImageUrl(movie.backdrop_path)} className="absolute brightness-50" alt="..." />
-              <div className="absolute mt-[80px] h-[400px] w-1/2 flex flex-col gap-8 justify-center items-start px-32">
-                <span className="font-bold text-5xl text-white">{movie.title}</span>
-                <span className="text-white overflow-hidden">{movie.overview}</span>
+              <img src={GetImageUrl(movie.backdrop_path)} className="absolute h-[600px] w-full brightness-50" alt="..." />
+              <div className="absolute h-full w-1/2 flex flex-col gap-8 justify-center px-32">
+                <div className="font-bold text-4xl text-white">{movie.title}</div>
+                <div className="text-white">{movie.overview}</div>
+                <div className="flex gap-2">
+                  {
+                    movie.genre_ids.map(genreId => 
+                      <div className="text-white bg-gray-300/50 rounded-lg px-2">
+                        {Genres.get(genreId)}
+                      </div>
+                    )
+                  }
+                </div>                
               </div>
             </SwiperSlide>)
         }
--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -3,7 +3,7 @@ import { API_KEY } from "./apiKey";
 
 const ImagesBaseURL: string = "https://image.tmdb.org/t/p/original"
 
-export const GetImageUrl = (imgPath: string): string  => ImagesBaseURL + imgPath;
+export const GetImageUrl = (imgPath: string): string  => ImagesBaseURL + imgPath
 
 export const FetchUpcomingMovies = async () => {
   const response = await fetch(`https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1&api_key=${API_KEY}`)
@@ -12,4 +12,24 @@ export const FetchUpcomingMovies = async () => {
   return movies
 }
 
-
+export const Genres = new Map<number, string>([
+  [28, "Action"],
+  [12, "Adventure"],
+  [16, "Animation"],
+  [35, "Comedy"],
+  [80, "Crime"],
+  [99, "Documentary"],
+  [18, "Drama"],
+  [10751, "Family"],
+  [14, "Fantasy"],
+  [36, "History"],
+  [27, "Horror"],
+  [10402, "Music"],
+  [9648, "Mystery"],
+  [10749, "Romance"],
+  [878, "Science Fiction"],
+  [10770, "TV Movie"],
+  [53, "Thriller"],
+  [10752, "War"],
+  [37, "Western"]
+])
