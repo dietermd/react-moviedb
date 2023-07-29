d305bf357c7652989ed00a55c1a4ce6d289eee34
Dieter Mutiz <dieter_md@hotmail.com>
2023-07-28 22:41:18 -0300

    Header and banner reponsive

--- src/components/bannner.tsx
+++ src/components/bannner.tsx
@@ -10,10 +10,37 @@ import 'swiper/css/pagination';
 export default function Banner() {
   const [movies, setMovies] = useState<Movie[]>([])  
 
-  useEffect(() => {
-    FetchUpcomingMovies()
-    .then(movies => setMovies(movies))
-  }, [])
+  // useEffect(() => {
+  //   FetchUpcomingMovies()
+  //   .then(movies => setMovies(movies))
+  // }, [])
+
+  useEffect(() =>
+    setMovies([
+      {
+        "adult": false,
+        "backdrop_path": "/2vFuG6bWGyQUzYS9d69E5l85nIz.jpg",
+        "genre_ids": [
+          28,
+          12,
+          878
+        ],
+        "id": 667538,
+        "original_language": "en",
+        "original_title": "Transformers: Rise of the Beasts",
+        "overview": "When a new threat capable of destroying the entire planet emerges, Optimus Prime and the Autobots must team up with a powerful faction known as the Maximals. With the fate of humanity hanging in the balance, humans Noah and Elena will do whatever it takes to help the Transformers as they engage in the ultimate battle to save Earth.",
+        "popularity": 4835.711,
+        "poster_path": "/gPbM0MK8CP8A174rmUwGsADNYKD.jpg",
+        "release_date": "2023-06-06",
+        "title": "Transformers: Rise of the Beasts",
+        "video": false,
+        "vote_average": 7.5,
+        "vote_count": 1940
+      }
+    ])
+  ,[])
+
+  
 
 
   return (
@@ -38,17 +65,17 @@ function RenderMovieCarousel(movies: Movie[]) {
         {
           movies.map((movie, i) =>
             <SwiperSlide key={i}>
-              <div className="absolute h-full w-1/2 flex flex-col gap-4 justify-center px-4 z-10">
+              <div className="absolute h-full w-full md:w-1/2 flex flex-col gap-4 justify-center px-4 z-10">
                 <div className="font-bold text-lg text-red-500">{`#${i + 1} Upcoming`}</div>
                 <div className="font-bold text-4xl text-white">{movie.title}</div>
-                <div className="flex gap-2">
+                <div className="hidden md:flex gap-2">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
                   </svg>
                   <div className="text-white">{new Date(movie.release_date).toLocaleDateString('en-us', {year:"numeric", month:"short", day:"numeric"})}</div>
                 </div>
-                <div className="text-white">{movie.overview}</div>
-                <div className="flex gap-2">
+                <div className="hidden md:flex text-white">{movie.overview}</div>
+                <div className="hidden md:flex gap-2">
                   {
                     movie.genre_ids.map(genreId => 
                       <div className="text-white border-2 border-white rounded-lg px-2">
@@ -68,7 +95,7 @@ function RenderMovieCarousel(movies: Movie[]) {
                 </div>
               </div>
 
-              <div className="banner-image-cover relative h-full w-3/4 float-right">
+              <div className="banner-image-cover relative h-full w-full md:w-3/4 float-right">
                 <div className="banner-inner-image-cover h-full w-full">
                   <img src={GetImageUrl(movie.backdrop_path)} className="absolute h-full w-full object-cover mix-blend-overlay" alt="..." />
                 </div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,23 +4,33 @@ export default function Header() {
     <header className="relative w-full z-10">
       <nav className="absolute h-[80px] w-full flex items-center px-6 bg-gradient-to-b from-[#242428]">          
         <div className="basis-1/12 h-full flex items-center gap-2">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="red" className="w-6 h-6">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="red" className="w-8 h-8">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 01-1.125-1.125M3.375 19.5h1.5C5.496 19.5 6 18.996 6 18.375m-3.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-1.5A1.125 1.125 0 0118 18.375M20.625 4.5H3.375m17.25 0c.621 0 1.125.504 1.125 1.125M20.625 4.5h-1.5C18.504 4.5 18 5.004 18 5.625m3.75 0v1.5c0 .621-.504 1.125-1.125 1.125M3.375 4.5c-.621 0-1.125.504-1.125 1.125M3.375 4.5h1.5C5.496 4.5 6 5.004 6 5.625m-3.75 0v1.5c0 .621.504 1.125 1.125 1.125m0 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m1.5-3.75C5.496 8.25 6 7.746 6 7.125v-1.5M4.875 8.25C5.496 8.25 6 8.754 6 9.375v1.5m0-5.25v5.25m0-5.25C6 5.004 6.504 4.5 7.125 4.5h9.75c.621 0 1.125.504 1.125 1.125m1.125 2.625h1.5m-1.5 0A1.125 1.125 0 0118 7.125v-1.5m1.125 2.625c-.621 0-1.125.504-1.125 1.125v1.5m2.625-2.625c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125M18 5.625v5.25M7.125 12h9.75m-9.75 0A1.125 1.125 0 016 10.875M7.125 12C6.504 12 6 12.504 6 13.125m0-2.25C6 11.496 5.496 12 4.875 12M18 10.875c0 .621-.504 1.125-1.125 1.125M18 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m-12 5.25v-5.25m0 5.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125m-12 0v-1.5c0-.621-.504-1.125-1.125-1.125M18 18.375v-5.25m0 5.25v-1.5c0-.621.504-1.125 1.125-1.125M18 13.125v1.5c0 .621.504 1.125 1.125 1.125M18 13.125c0-.621.504-1.125 1.125-1.125M6 13.125v1.5c0 .621-.504 1.125-1.125 1.125M6 13.125C6 12.504 5.496 12 4.875 12m-1.5 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M19.125 12h1.5m0 0c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h1.5m14.25 0h1.5" />
           </svg>
           <span className="font-bold text-white">MovieDB</span>
         </div>
 
-        <div className="basis-10/12 h-full flex justify-center items-center">
-          <input type="text" className="w-1/2 h-1/2 bg-transparent border-2 border-white rounded-lg outline-none text-center text-white placeholder:text-white" placeholder="Search movie..."/>
+        <div className="basis-10/12 h-full hidden md:flex justify-center items-center">
+          <input type="text" className="w-3/4 lg:w-2/3 xl:w-3/5  h-1/2 bg-transparent border-2 border-white rounded-lg outline-none text-center text-white placeholder:text-white" placeholder="Search movie..."/>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="white" 
             className="pointer-events-none w-5 h-5 -translate-x-8">
             <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
           </svg>
         </div>
 
+        <div className="basis-10/12 h-full flex md:hidden justify-end items-center cursor-pointer pr-4">
+          <div className="flex justify-center items-center w-[42px] h-1/2 bg-transparent border-2 border-white rounded-lg outline-none">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="white" 
+              className="pointer-events-none w-5 h-5">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+            </svg>
+          </div>
+          
+        </div>
+
         <div className="basis-1/12 h-full flex items-center justify-end">
           <a href="https://github.com/dietermd/react-moviedb" target="_blank">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="red"  viewBox="0 0 24 24" strokeWidth={2} stroke="none" className="w-6 h-6">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="red"  viewBox="0 0 24 24" strokeWidth={2} stroke="none" className="w-8 h-8">
               <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
             </svg>
           </a>
