44bab48f771ab695e8665d37c8db7296517dda2e
Dieter Mutiz <dieter_md@hotmail.com>
2023-08-01 22:21:17 -0300

    MovieCard component

---
+++ src/components/movieCard.tsx
@@ -0,0 +1,28 @@
+import { GetFormatedDate, GetImageUrl } from "../utils/utils"
+
+interface MovieCardProps {
+  title: string
+  release_date: string
+  poster_path: string
+}
+
+
+export default function MovieCard(props: MovieCardProps) {  
+  
+  const posterUrl = GetImageUrl(props.poster_path)
+
+  return(
+    <>
+      <div className="flex flex-col w-[150px]">
+        <a href="#">
+          <img src={posterUrl} className="w-full h-[225px] rounded-lg" />          
+        </a>
+        <div className="mt-2 px-[10px] text-sm">
+          <div className="font-bold text-white">{props.title}</div>
+          <div className="text-white/50">{GetFormatedDate(props.release_date)}</div>
+        </div>
+        
+      </div>
+    </>
+  )
+}
