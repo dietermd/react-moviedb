536c3c8d959c2c406457d5fa1f3f37d2f9aa0f74
Dieter Mutiz <dieter_md@hotmail.com>
2023-08-15 23:10:49 -0300

    Render MovieCard only after image loaded

--- src/components/movieCard.tsx
+++ src/components/movieCard.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react"
 import { GetFormatedDate, GetImageUrl, ImageSizes } from "../utils/utils"
+import MovieCardLoading from "./loading/movieCardLoading"
 
 interface MovieCardProps {
   title: string
@@ -9,19 +11,33 @@ interface MovieCardProps {
 export default function MovieCard(props: MovieCardProps) {  
   
   const posterUrl = GetImageUrl(props.poster_path, ImageSizes.W500)
+  const [imgLoaded, setImgLoaded] = useState(false);
 
-  return(
-    <>
-      <div className="flex flex-col w-[150px]">
-        <a href="#">
-          <img src={posterUrl} className="w-full h-[225px] rounded-lg" />          
-        </a>
-        <div className="mt-2 px-[10px] text-sm text-center">
-          <div className="font-bold text-white">{props.title}</div>
-          <div className="text-white/50">{GetFormatedDate(props.release_date)}</div>
-        </div>
-        
-      </div>
-    </>
+  useEffect(() => {
+    const img = new Image()
+    img.onload = () => setImgLoaded(true)
+    img.src = posterUrl
+  }, [])
+
+  console.log(props.title)
+
+  return (
+    imgLoaded ? RenderMovieCard() : <MovieCardLoading />
   )
+
+  function RenderMovieCard() {
+    return (
+      <>
+        <div className="flex flex-col w-[150px]">
+          <a href="#">
+            <img src={posterUrl} className="w-full h-[225px] rounded-lg" />          
+          </a>
+          <div className="mt-2 px-[10px] text-sm text-center">
+            <div className="font-bold text-white">{props.title}</div>
+            <div className="text-white/50">{GetFormatedDate(props.release_date)}</div>
+          </div>        
+        </div>
+      </>
+    )
+  }
 }
