f88df3f28062f3a36f7b97abbbed2f5ed3b83cfa
Dieter Mutiz <dieter_md@hotmail.com>
2023-09-10 21:24:13 -0300

    Search bar and modal size

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -25,11 +25,11 @@ export default function Search() {
     <>
       <div className="relative basis-8/12 h-full hidden md:flex justify-center items-center">
         <input type="text" 
-          className="absolute w-3/4 lg:w-2/3 xl:w-3/5 h-1/2 bg-transparent border-2 border-white rounded-lg outline-none text-center text-white placeholder:text-white/50"
+          className="absolute w-5/6 lg:w-2/3 xl:w-3/5 h-1/2 bg-transparent border-2 border-white rounded-lg outline-none text-center text-white placeholder:text-white/50"
           placeholder="Search movie..."
           onChange={(event) => setSearch(event.target.value)}
         />
-        <div className="w-3/4 lg:w-2/3 xl:w-3/5 md:flex justify-end pr-2">
+        <div className="w-5/6 lg:w-2/3 xl:w-3/5 md:flex justify-end pr-2">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="white" 
             className="pointer-events-none w-5 h-5">
             <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
@@ -37,7 +37,7 @@ export default function Search() {
         </div>          
       </div>
 
-      <div className="basis-8/12 h-full md:hidden flex justify-end items-center cursor-pointer pr-4">
+      <div className="basis-8/12 h-full md:hidden flex justify-end items-center cursor-pointer">
         <div className="flex justify-center items-center w-[42px] h-1/2 bg-transparent border-2 border-white rounded-lg outline-none">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="white" 
             className="pointer-events-none w-5 h-5">
--- src/components/search/searchModal.tsx
+++ src/components/search/searchModal.tsx
@@ -9,8 +9,8 @@ export default function SearchModal(props: SearchModalProps) {
   return (
     <>
       <div className="absolute inset-x-0 top-[70px] px-6 flex justify-center">
-        <div className="basis-8/12 flex justify-center">
-          <div className="flex flex-col w-3/4 lg:w-2/3 xl:w-3/5 max-h-[70vh] overflow-auto bg-white rounded-lg">
+        <div className="md:basis-8/12 flex justify-center">
+          <div className="flex flex-col md:w-5/6 lg:w-2/3 xl:w-3/5 max-h-[70vh] overflow-auto bg-white rounded-lg">
             {
               props.movies.map((movie, i) => <SearchModalMovieCard movie={movie} key={i} />)
             }
