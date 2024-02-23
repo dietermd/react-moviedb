eb2a903275894de86e02b774357a9558896ef25e
Dieter <dieter_md@hotmail.com>
2024-02-22 21:54:30 -0300

    Resposive person page banner

--- src/routes/person/personPageBanner.tsx
+++ src/routes/person/personPageBanner.tsx
@@ -7,13 +7,13 @@ export default function PersonPageBanner(props: {personDetails: PersonDetails}) 
 
   return (
     <>
-      <div className="relative h-[600px] w-full">
-        <div className="absolute flex w-full h-full gap-8">
-          <div className="w-full md:w-1/3 flex justify-center items-center backdrop-brightness-125">
+      <div className="relative h-[900px] md:h-[600px] w-full">
+        <div className="absolute flex flex-col md:flex-row w-full h-full md:gap-8">
+          <div className="h-[600px] w-full md:w-1/3 flex justify-center items-center backdrop-brightness-125">
             <img src={profileUrl} className="w-[250px] h-[400px] rounded-lg" />
           </div>
 
-          <div className="w-full md:w-2/3 hidden md:flex md:flex-col gap-4 justify-center text-white pr-4">
+          <div className="h-[300px] md:h-[600px] w-full md:w-2/3 flex flex-col gap-4 justify-center text-white pr-4">
             <div className="font-bold text-4xl">{personDetails.name}</div>
             <div className="line-clamp-[10]">{personDetails.biography}</div>
           </div>
